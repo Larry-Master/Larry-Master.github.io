66ad39255debd1adeb0850671e02a10c25c2cae0
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-04-24 15:59:20 +0200

    Handlebar + Static Pages

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { engine } = require("express-handlebars");
+
 const server = express();
 const port = 3001;
+
+server.use(express.static('public'));
 
 server.get("/", function (req, res) {
     res.send("Hello Prof");
